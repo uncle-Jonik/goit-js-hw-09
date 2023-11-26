aaab671b37305dcd70ac22b6a5267816f42d6d6e
uncle-Jonik <126769681+uncle-Jonik@users.noreply.github.com>
2023-11-26 00:19:23 +0000

    Deploying to gh-pages from @ uncle-Jonik/goit-js-hw-09@c34453f889744cdfbab6b93502245600da4d09ae 🚀

---
+++ 01-color-switcher.5ab1d0be.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0;let d=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.querySelector("body").style.backgroundColor=t,document.querySelector("body").style.transition="250ms cubic-bezier(0.4, 0, 0.2, 1)"}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.5ab1d0be.js.map
