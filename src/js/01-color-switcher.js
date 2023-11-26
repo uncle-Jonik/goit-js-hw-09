c34453f889744cdfbab6b93502245600da4d09ae
uncle-Jonik <Katsimon_Eu@outlook.com>
2023-11-26 02:18:01 +0200

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+function getRandomHexColor() {
+  // рандомайзер кольору
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+stopButton.disabled = true;
+
+let timer = null;
+
+startButton.addEventListener('click', onStartChangeBackgroundColor);
+stopButton.addEventListener('click', onStopChangeBackgroundColor);
+//
+//
+//
+//функція по зміні кольору
+function onStartChangeBackgroundColor() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+
+  timer = setInterval(() => {
+    const currentColor = getRandomHexColor();
+    document.querySelector('body').style.backgroundColor = currentColor;
+    document.querySelector('body').style.transition =
+      '250ms cubic-bezier(0.4, 0, 0.2, 1)';
+  }, 1000);
+}
+//
+//припинення зміни кольору
+function onStopChangeBackgroundColor() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+
+  clearInterval(timer);
+}
