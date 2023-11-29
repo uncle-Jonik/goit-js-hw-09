cf63ca708488d098ed8c15232425536393770aa1
uncle-Jonik <Katsimon_Eu@outlook.com>
2023-11-29 11:19:28 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,81 +1,74 @@
-const flatpickr = require('flatpickr');
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
-//
-//
+
+// посилання на елементи
 const refs = {
   input: document.querySelector('input#datetime-picker'),
-  startButton: document.querySelector('button[data-start]'),
+  startBtn: document.querySelector('button[data-start]'),
   dataDays: document.querySelector('span[data-days]'),
   dataHours: document.querySelector('span[data-hours]'),
   dataMinutes: document.querySelector('span[data-minutes]'),
   dataSeconds: document.querySelector('span[data-seconds]'),
 };
-strBtn = refs.startButton;
-strBtn.disabled = true;
-//
+let timerId = null;
+let startTime = null;
+let selectedTime = null;
 
-//
-//
-//
-function pad(value) {
-  return String(value).padStart(2, '0');
+refs.startBtn.addEventListener('click', handleStartBtnClick);
+refs.startBtn.setAttribute('disabled', 'disabled');
+
+function handleStartBtnClick() {
+  timerId = setInterval(() => {
+    startTime = Date.now();
+    const currentTime = selectedTime - startTime;
+    const time = convertMs(currentTime);
+    console.log(time);
+    updateTime(time);
+    if (currentTime < 1000) {
+      clearInterval(timerId);
+    }
+  }, 1000);
 }
-//
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
+// ms в об'єкт
+function convertMs(time) {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
-  const days = pad(Math.floor(ms / day));
-  // Remaining hours
-  const hours = pad(Math.floor((ms % day) / hour));
-  // Remaining minutes
-  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
-  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+  const days = pad(Math.floor(time / day));
+  const hours = pad(Math.floor((time % day) / hour));
+  const minutes = pad(Math.floor(((time % day) % hour) / minute));
+  const seconds = pad(Math.floor((((time % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
-//
-//
-//
-//
-const options = {
+// запуск функції
+flatpickr('input#datetime-picker', {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    // Чи вірно введений час?
-    function correctlyEnteredTime() {
-      if (new Date() >= selectedDates[0]) {
-        return alert('Please choose a date in the future');
-      }
-      strBtn.disabled = false;
+    selectedTime = selectedDates[0];
+
+    if (selectedTime > Date.now()) {
+      refs.startBtn.removeAttribute('disabled');
+    } else {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      refs.startBtn.setAttribute('disabled', 'disabled');
     }
-    correctlyEnteredTime();
-    //
-    //
-    strBtn.addEventListener('click', () => {
-      strBtn.disabled = true;
-      setInterval(() => {
-        const currentTime = Date.now();
-        const deltaTime = selectedDates[0] - currentTime;
-        //
-        const { days, hours, minutes, seconds } = convertMs(deltaTime);
-        refs.dataDays.textContent = days;
-        refs.dataHours.textContent = hours;
-        refs.dataMinutes.textContent = minutes;
-        refs.dataSeconds.textContent = seconds;
-      }, 1000);
-    });
   },
-};
-//
-//
-flatpickr(refs.input, options);
+});
+// рефракторинг виводу
+function updateTime({ days, hours, minutes, seconds }) {
+  refs.dataDays.textContent = days;
+  refs.dataHours.textContent = hours;
+  refs.dataMinutes.textContent = minutes;
+  refs.dataSeconds.textContent = seconds;
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
