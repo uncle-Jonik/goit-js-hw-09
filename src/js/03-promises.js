d3230e1685b35d79b7c083bbe30e4381e412be31
uncle-Jonik <Katsimon_Eu@outlook.com>
2023-11-30 15:58:25 +0200

    logic 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,25 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+// import Notiflix from 'notiflix';
+
+setInterval(() => {
+  function createPromise(delay) {
+    return new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve('✅ Fulfilled promise');
+        }
+        reject('❌ Rejected promise');
+      }, delay);
+    });
   }
-}
+  createPromise(2000) // Кількість ms до початку
+    .then(result => {
+      console.log(result);
+    })
+    .catch(result => {
+      console.log(result);
+    });
+}, 1000); // кількість ms до наступного
+
+// логіка працює, залишилося взяти данні з полів форми
