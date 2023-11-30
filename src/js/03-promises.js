88be8230c84a186e2822cce9f85724900f0a56eb
uncle-Jonik <Katsimon_Eu@outlook.com>
2023-11-30 23:31:42 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,25 +1,41 @@
 // import Notiflix from 'notiflix';
+const form = document.querySelector('.form');
 
-setInterval(() => {
-  function createPromise(delay) {
-    return new Promise((resolve, reject) => {
-      const shouldResolve = Math.random() > 0.3;
+form.addEventListener('submit', onCreatePromisesClick);
 
-      setTimeout(() => {
-        if (shouldResolve) {
-          resolve('✅ Fulfilled promise');
+function onCreatePromisesClick(evt) {
+  evt.preventDefault();
+
+  let counter = 0;
+  const firstDelay = Number(evt.target[0].value);
+  const delayStep = Number(evt.target[1].value);
+  const amount = Number(evt.target[2].value);
+  // console.log(amount);
+
+  const timerId = setInterval(() => {
+    function createPromise(delay) {
+      return new Promise((resolve, reject) => {
+        counter += 1;
+        const shouldResolve = Math.random() > 0.3;
+
+        setTimeout(() => {
+          if (shouldResolve) {
+            resolve('✅ Fulfilled promise');
+          }
+          reject('❌ Rejected promise');
+        }, delay);
+
+        if (counter === amount) {
+          clearInterval(timerId);
         }
-        reject('❌ Rejected promise');
-      }, delay);
-    });
-  }
-  createPromise(2000) // Кількість ms до початку
-    .then(result => {
-      console.log(result);
-    })
-    .catch(result => {
-      console.log(result);
-    });
-}, 1000); // кількість ms до наступного
-
-// логіка працює, залишилося взяти данні з полів форми
+      });
+    }
+    createPromise(firstDelay)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(result => {
+        console.log(result);
+      });
+  }, delayStep);
+}
